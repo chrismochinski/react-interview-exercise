6b44eca288c504c540e838a981f29dc858cfbf87
Chris "Mo" Mochinski <cmochinski@gmail.com>
2021-11-07 15:05:56 -0600

    map / details page
    progress with header, lat/lon and placeholder text pre-map

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { Search2Icon } from "@chakra-ui/icons";
+import { Search2Icon, InfoIcon } from "@chakra-ui/icons";
+
+import Map from "./Map";
 
 import {
+  Container,
   Button,
   Center,
   Heading,
@@ -20,6 +23,10 @@ import {
   InputRightElement,
   Flex,
   Stack,
+  Grid,
+  SimpleGrid,
+  Box,
+  IconButton,
 } from "@chakra-ui/react";
 import { Card } from "@components/design/Card";
 import {
@@ -28,9 +35,10 @@ import {
   NCESDistrictFeatureAttributes,
   NCESSchoolFeatureAttributes,
 } from "@utils/nces";
+import { extraInfo } from "@utils/extraInfo"; // for props / separate in case I want to adjust
 
 const Home: React.FC = () => {
-  const [searching, setSearching] = useState(false); //for loading
+  const [searching, setSearching] = useState(false);
   const [districtSearch, setDistrictSearch] = useState<
     NCESDistrictFeatureAttributes[] //district search
   >([]);
@@ -38,11 +46,11 @@ const Home: React.FC = () => {
     NCESSchoolFeatureAttributes[] //school search
   >([]);
 
+  const [userDistrictInput, setUserDistrictInput] = useState(""); // user input search string
+  // const [selectionLat, setSelectionLat] = useState<extraInfo>(); // longitude of selection (this may go away)
+  // const [selectionLon, setSelectionLon] = useState<extraInfo>(); // latitude of selection (this may go away)
+  const [selection, setSelection] = useState<extraInfo>({}); //when user clicks the icon for more info
 
-
-  const [userDistrictInput, setUserDistrictInput] = useState("");
-
-  
   const handleDistrictClick = async (event: any) => {
     event.preventDefault();
     if (!userDistrictInput) {
@@ -51,115 +59,154 @@ const Home: React.FC = () => {
       setSearching(true);
       console.log("in handleDistrictClick, searching:", userDistrictInput);
 
-      //updated paste from demo
       const demoDistrictSearch = await searchSchoolDistricts(
-        // "Peninsula School District" (demo data)
-        userDistrictInput //user input variable
-
+        userDistrictInput //user input
       );
       setDistrictSearch(demoDistrictSearch);
-      console.log("List of district responses", demoDistrictSearch);
-  
+      console.log("List of district responses:", demoDistrictSearch);
+
       const demoSchoolSearch = await searchSchools(
         "k",
         demoDistrictSearch[0].LEAID
       );
       setSchoolSearch(demoSchoolSearch);
-     
 
-      console.log("List of associated", demoSchoolSearch);
-
-   //updated 
-
+      console.log("List of associated schools:", demoSchoolSearch);
     }
+    resultHeader();
     setSearching(false);
+  };
+
+  const resultHeader = () => {
+    if (0 === districtSearch.length) {
+      return `Let's run a search!`;
+    } else if (1 === districtSearch.length) {
+      return `1 District Result for "${userDistrictInput}"`;
+    } else if (100 < districtSearch.length) {
+      console.log("fire error - please narrow search");
+      return (
+        <Text className="too-many-results-header">
+          Too Many Results
+          <br />{" "}
+          <span style={{ fontSize: "16px" }}>Please Narrow Your Search</span>
+        </Text>
+      );
+      return;
+    } else {
+      return `${districtSearch.length} results for "${userDistrictInput}"`;
+    }
+  };
+
+  const displayExtraInfo = (selectedObject: any) => {
+    console.log("clicked", selectedObject.NAME);
+    if (undefined === selectedObject.LAT || undefined === selectedObject.LON) {
+      console.log("sorry - location service unavailable");
+    } else {
+      console.log("Latitude:", selectedObject.LAT);
+      console.log("Longitude:", selectedObject.LON);
+      // setSelectionLat(selectedObject.LAT);
+      // setSelectionLon(selectedObject.LON);
+      setSelection(selectedObject);
+    }
   };
 
   return (
     <Center padding="100px" height="90vh">
-      <ScaleFade initialScale={0.9} in={true}>
-        <Card variant="rounded" borderColor="blue">
-          <Heading align="center" textTransform="uppercase" fontWeight="600">
-            School Data Finder
-          </Heading>
-          <Text fontWeight="300">
-            Welcome to my little search function!
-            <br />
-            I didn't know Chakra before I started this, so I thought I'd give it
-            a shot...since I love learning and getting better and am even
-            willing to get out of my comfort zone and do this during a big coding
-            challenge
-            <br />
-          </Text>
-          <Divider style={{ margin: "10px 0" }} />
-         
+      <Container maxW="container.lg">
+        <ScaleFade initialScale={0.9} in={true}>
+          <Card variant="rounded" borderColor="blue">
+            <Heading align="center" textTransform="uppercase" fontWeight="600">
+              School Data Finder
+            </Heading>
 
-          <form onSubmit={() => handleDistrictClick(event)}>
-            <HStack spacing="24px">
-              <InputGroup>
-                <Input
-                  placeholder="Search for a District"
-                  size="md"
-                  onChange={(event) => setUserDistrictInput(event.target.value)}
+            <Divider style={{ margin: "10px 0" }} />
+
+            <form onSubmit={() => handleDistrictClick(event)}>
+              <HStack spacing="24px">
+                <InputGroup>
+                  <Input
+                    placeholder="Search for a District"
+                    value={userDistrictInput}
+                    size="md"
+                    onChange={(event) =>
+                      setUserDistrictInput(event.target.value)
+                    }
+                  />
+                  <InputRightElement
+                    children={<Search2Icon color="#0070ac43" />}
+                  />
+                </InputGroup>
+                <Button type="submit" size="md" variant="ghost">
+                  Search
+                </Button>
+              </HStack>
+            </form>
+
+            <SimpleGrid columns={2} spacing={10}>
+              <Box>
+                <Text fontWeight="400">
+                  {searching ? <Spinner /> : <></>}
+
+                  <Text>{resultHeader()}</Text>
+
+                  <br />
+
+                  {50 < districtSearch.length ? (
+                    <></>
+                  ) : (
+                    <UnorderedList>
+                      {districtSearch.map((result) => (
+                        <ListItem fontWeight="200" key={result.NAME}>
+                          {result.NAME}{" "}
+                          <InfoIcon
+                            className="i-icon"
+                            color="#DDB94F85"
+                            onClick={() => displayExtraInfo(result)}
+                          />
+                        </ListItem>
+                      ))}
+                    </UnorderedList>
+                  )}
+                </Text>
+                <br />
+
+                <Text fontWeight="400">
+                  {searching ? <Spinner /> : <></>}
+
+                  {(!searching && schoolSearch.length < 1) ||
+                  50 < districtSearch.length ? (
+                    <></>
+                  ) : (
+                    <Text className="schoolResultsTitle">
+                      {schoolSearch.length} Associated School Results
+                    </Text>
+                  )}
+                  <br />
+                  <UnorderedList>
+                    {schoolSearch.map((result) => (
+                      <ListItem fontWeight="200" key={result.NAME}>
+                        {result.NAME}{" "}
+                        <InfoIcon
+                          color="#DDB94F85"
+                          className="i-icon"
+                          onClick={() => displayExtraInfo(result)}
+                        />
+                      </ListItem>
+                    ))}
+                  </UnorderedList>
+                </Text>
+              </Box>
+              <Box w="100%">
+                <Map
+                  lat={selection.LAT}
+                  lon={selection.LON}
+                  name={selection.NAME}
                 />
-                <InputRightElement
-                  children={<Search2Icon color="#0070ac43" />}
-                />
-              </InputGroup>
-              <Button type="submit" size="md" variant="ghost">
-                Search
-              </Button>
-            </HStack>
-          </form>
-          <Text fontWeight="400">
-            {searching ? <Spinner /> : <></>}
-
-
-
-
-            {!searching && schoolSearch.length < 1 ? (
-              <Text>Let's search for districts!</Text>
-            ) : (
-              <Text>{districtSearch.length} District Results</Text>
-            )}
-
-
-
-
-            {/* {districtSearch.length} Demo Districts! */}
-            <br />
-            <UnorderedList>
-              {districtSearch.map((result) => (
-                <ListItem fontWeight="200" key={result.NAME}>
-                  {result.NAME}
-                </ListItem>
-              ))}
-            </UnorderedList>
-          </Text>
-
-
-          <Text fontWeight="400">
-            {searching ? <Spinner /> : <></>}
-            {!searching && schoolSearch.length < 1 ? (
-              <></>
-            ) : (
-              <Text className="schoolResultsTitle">{schoolSearch.length} School Results</Text>
-            )}
-            {/* {schoolSearch.length} Demo Schools! */}
-            <br />
-            <UnorderedList>
-              {schoolSearch.map((result) => (
-                <ListItem fontWeight="200" key={result.NAME}>
-                  {result.NAME}
-                </ListItem>
-              ))}
-            </UnorderedList>
-          </Text>
-
-
-
-        </Card>
-      </ScaleFade>
+              </Box>
+            </SimpleGrid>
+          </Card>
+        </ScaleFade>
+      </Container>
     </Center>
   );
 };
---
+++ src/utils/extraInfo.ts
@@ -0,0 +1,16 @@
+export interface extraInfo {
+    NCESSCH?: string,
+    LEAID?: string,
+    NAME?: string,
+    OPSTFIPS?: string,
+    STREET?: string,
+    CITY?: string,
+    STATE?: string,
+    ZIP?: string,
+    STFIP?: string,
+    CNTY?: string,
+    NMCNTY?: string,
+    LOCALE?: string,
+    LAT?: number,
+    LON?: number
+}
